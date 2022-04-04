deeb1d90624b6180045a39278fa65457d42a6062
Franco Luzardi <69637692+Ch1mu@users.noreply.github.com>
2022-04-04 15:33:14 -0300

    Add files via upload

--- Dia1Tarde/src/app/Program.java
+++ Dia1Tarde/src/app/Program.java
@@ -7,16 +7,17 @@ import classes.Triangle;
 public class Program {
 
 	public static void main(String[] args) {
-		Triangle triangle = new Triangle(5,5,5);
-		System.out.println(triangle.getType());
 		
-	System.out.println(	isPrime() );
+		//Triangle triangle = new Triangle(5,5,5);
+		//System.out.println(triangle.getType());
+		
+		//System.out.println(isPrime());
 
 	}
 
 	//2 exercise, prime numbers
 	
-	public static boolean isPrime()
+	public static boolean isPrimeIf()
 	{
 		
 		Scanner scan = new Scanner(System.in);
@@ -32,7 +33,7 @@ public class Program {
 		
 		}while (number >14 || number < 1);
 		
-		if (number == 1) //that numbers aren't prime, but they are only divisible between 1 and themself
+		if (number == 1) //this number isn't prime, but it's  only divisible between 1 and themself
 		 {
 			    return false;
 		 }
@@ -43,6 +44,41 @@ public class Program {
 				 return false;
 			 }
 		 }
+		
+		return true;
+	}
+	
+	
+	public static boolean isPrimeSwitch()
+	{
+		
+		Scanner scan = new Scanner(System.in);
+		int number;
+		
+		do 
+		{ // do while because i need to check that is a number between 1 and 14
+				
+		System.out.println(" \n Enter a number beetwen 1 and 14: ");
+		number = scan.nextInt();
+		if(number >14 || number < 1)
+			System.out.println("that number doesn't respect the conditions ");
+		
+		}while (number >14 || number < 1);
+		
+		switch (number)
+		{
+		case 1:    return false;  //this number isn't prime, but it's  only divisible between 1 and themself
+		
+		default: //default = else
+		 for(int i = 2; i<number; i++) 
+		 {
+			 if(number%i == 0) //here I look if they are divisible by a number between 2 and the number
+			 {
+				 return false;
+			 }
+		 }
+		 break;
+		}
 		
 		return true;
 	}
