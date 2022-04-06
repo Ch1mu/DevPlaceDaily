4fc377d03ba1d66e899553c0093c65b681b42339
Franco Luzardi <69637692+Ch1mu@users.noreply.github.com>
2022-04-06 17:11:52 -0300

    Add files via upload

--- Dia3/3EjerciciosPropios/src/interfaces/CombatInterface.java
+++ Dia3/3EjerciciosPropios/src/interfaces/CombatInterface.java
@@ -5,4 +5,7 @@ import classes.Character;
 public interface CombatInterface {
 
 	public double getPower();
+	public void setPower(double power);
+	public String getSableColor();
+	public void setSableColor(String sableColor);
 }
